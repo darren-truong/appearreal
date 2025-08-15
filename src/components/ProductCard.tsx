db92bab5a73d66e5bbf514403d49cddbb17dd312
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-08-14 19:17:31 -0700

    Zod validation & type inference

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,8 +2,9 @@ import Link from "next/link";
 import { Card, CardContent, CardHeader } from "./ui/card";
 import Image from "next/image";
 import ProductPrice from "./ProductPrice";
+import { Product } from "@/lib/types";
 
-export default function ProductCard({ product }: { product: any }) {
+export default function ProductCard({ product }: { product: Product }) {
   return (
     <Card className="w-full max-w-sm">
       <CardHeader className="p-0 items-center">
