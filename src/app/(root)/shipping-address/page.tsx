6f0725c9948fcc36e03db028e78bc88326748510
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-08-17 08:25:07 -0700

    Shipping address zod schema & page

---
+++ src/app/(root)/shipping-address/page.tsx
@@ -0,0 +1,25 @@
+import { getMyCart } from "@/actions/cart.actions";
+import { getUserById } from "@/actions/user.actions";
+import { auth } from "@/auth";
+import { Metadata } from "next";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Shipping Address",
+};
+
+export default async function ShippingAddressPage() {
+  const cart = await getMyCart();
+
+  if (!cart || cart.items.length === 0) redirect("/cart");
+
+  const session = await auth();
+
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("No user ID");
+
+  const user = await getUserById(userId);
+
+  return <>Address</>;
+}
