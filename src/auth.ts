b98e45f1c1c6942cb99fe9e31e2c5f2a4a745bed
Darren Truong <128624091+darren-truong@users.noreply.github.com>
2025-08-17 09:44:01 -0700

    Protecting paths

--- src/auth.ts
+++ src/auth.ts
@@ -117,6 +117,23 @@ export const config = {
     },
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     authorized({ request, auth }: any) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname for req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protectedd path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // Check for session cart cookie
       if (!request.cookies.get("sessionCartId")) {
         // Generate new session cart id cookie
